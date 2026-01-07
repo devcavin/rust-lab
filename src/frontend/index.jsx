305264946f6d5a033e2f47cf1296095c62862dec
Cavin <cavinlarry001@gmail.com>
2026-01-08 00:55:51 +0300

    Added Jira API call

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,22 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text } from '@forge/react';
-import { invoke } from '@forge/bridge';
+import React from 'react';
+import ForgeReconciler, {Text, useProductContext} from "@forge/react"; // useProductContext hook for getting the context of the Atlassian app
+import {requestJira} from "@forge/bridge"; // the Jira REST API
+
 
 const App = () => {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-  }, []);
-  return (
-    <>
-      <Text>Hello world!</Text>
-      <Text>{data ? data : 'Loading...'}</Text>
-    </>
-  );
-};
+    const context = useProductContext();
+
+    const [comments, setComments] = React.useState([]);
+    console.log(`Number of comments on this issue: ${comments?.length}`);
+
+    // Jira issue call
+    const fetchCommentsForIssue = async () => {
+        // extracting issue id instead of getting from an input
+        const issueId = context?.extension.issue.id;
+        const response = await requestJira(`/rest/api/3/issue/${issueId}/comment`);
+        const data = await response.json();
+        return data?.comments;
+    }
+
+    React.useEffect(() => {
+        if (context) {
+            fetchCommentsForIssue().then(setComments)
+        }
+    }, [context]);
+
+    return <Text>Number of comments: {comments?.length}</Text>
+}
 
 ForgeReconciler.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
